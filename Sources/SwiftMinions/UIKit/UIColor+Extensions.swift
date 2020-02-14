93354633c2fc5e237291cc6fe71f8806eefc6230
stephen <tasb00429@gmail.com>
2020-02-14 22:20:46 +0800

    [ Add ]：UIColor+Extensions

---
+++ Sources/SwiftMinions/UIKit/UIColor+Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  Extension+UIColor.swift
+//  SwiftMinions
+//
+//  Created by stephen on 2020/2/14.
+//  Copyright © 2020 SwiftMinions. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    
+    /// SwifterSwift: Create UIColor from hexadecimal value with optional transparency.
+    ///
+    /// - Parameters:
+    ///   - hex: hex Int (example: 0xDECEB5).
+    ///   - transparency: optional transparency value (default is 1).
+    convenience init(hex: Int, transparency: CGFloat = 1) {
+        var trans: CGFloat {
+            if transparency > 1 {
+                return 1
+            } else if transparency < 0 {
+                return 0
+            } else {
+                return transparency
+            }
+        }
+        self.init(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff, transparency: trans)
+    }
+    
+    /// SwifterSwift: Create UIColor from RGB values with optional transparency.
+    ///
+    /// - Parameters:
+    ///   - red: red component.
+    ///   - green: green component.
+    ///   - blue: blue component.
+    ///   - transparency: optional transparency value (default is 1).
+    convenience init(red: Int, green: Int, blue: Int, transparency: CGFloat = 1) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        var trans: CGFloat {
+            if transparency > 1 {
+                return 1
+            } else if transparency < 0 {
+                return 0
+            } else {
+                return transparency
+            }
+        }
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: trans)
+    }
+}
