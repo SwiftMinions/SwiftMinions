ba42c0dc56a095efb5df0822ebc2777e768caeaf
ohlulu <z30262226@gmail.com>
2020-02-23 17:39:54 +0800

    [ Add ]：UIWindow

--- Sources/WaitMerge/OhluluExtensions.swift
+++ Sources/WaitMerge/OhluluExtensions.swift
@@ -710,6 +710,7 @@ public extension UIScreen {
         return UIScreen.main.bounds.width
     }
 }
+
 public extension Thread {
     static func mainThread(_ closure: @escaping () -> Void) {
         if Thread.isMainThread {
@@ -719,5 +720,56 @@ public extension Thread {
                 closure()
             }
         }
+    }
+}
+
+public extension UIWindow {
+    
+    static var safeAreaInset: UIEdgeInsets {
+        guard let window = UIApplication.shared.delegate?.window as? UIWindow else {
+            return .init(edge: 0)
+        }
+        
+        if #available(iOS 11.0, *) {
+            return window.safeAreaInsets
+        } else {
+            return .init(edge: 0)
+        }
+    }
+    
+    static var topVC: UIViewController? {
+        var rs: UIViewController? = nil
+        Thread.mainThread {
+            guard let window = UIApplication.shared.delegate?.window,
+                var topVC = window?.rootViewController else {
+                    return
+            }
+            while let presentedVC = topVC.presentedViewController {
+                topVC = presentedVC
+            }
+            rs = topVC
+        }
+        return rs
+    }
+    
+    static func switchRootViewController(
+        to viewController: UIViewController,
+        animated: Bool = true,
+        duration: TimeInterval = 0.5,
+        options: UIView.AnimationOptions = .transitionCrossDissolve,
+        _ completion: (() -> Void)? = nil) {
+        guard let _window = UIApplication.shared.delegate?.window,
+            let window = _window else {
+                return
+        }
+        
+        UIView.transition(with: window, duration: duration, options: options, animations: {
+            let oldState = UIView.areAnimationsEnabled
+            UIView.setAnimationsEnabled(false)
+            window.rootViewController = viewController
+            UIView.setAnimationsEnabled(oldState)
+        }, completion: { _ in
+            completion?()
+        })
     }
 }
