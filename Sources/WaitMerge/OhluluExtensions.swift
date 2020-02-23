d55fd5ace5e918419f3e6a999c44539ba5734492
ohlulu <z30262226@gmail.com>
2020-02-23 17:54:11 +0800

    [ Add ]：UINavigationController

--- Sources/WaitMerge/OhluluExtensions.swift
+++ Sources/WaitMerge/OhluluExtensions.swift
@@ -993,3 +993,32 @@ public extension UIViewController {
         child.removeFromParent()
     }
 }
+
+public extension UINavigationController {
+    
+    /// SwifterSwift: Pop ViewController with completion handler.
+    ///
+    /// - Parameters:
+    ///   - animated: Set this value to true to animate the transition (default is true).
+    ///   - completion: optional completion handler (default is nil).
+    func popViewController(animated: Bool = true, _ completion: (() -> Void)? = nil) {
+        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        popViewController(animated: animated)
+        CATransaction.commit()
+    }
+    
+    /// SwifterSwift: Push ViewController with completion handler.
+    ///
+    /// - Parameters:
+    ///   - viewController: viewController to push.
+    ///   - completion: optional completion handler (default is nil).
+    func pushViewController(_ viewController: UIViewController, completion: (() -> Void)? = nil) {
+        // https://github.com/cotkjaer/UserInterface/blob/master/UserInterface/UIViewController.swift
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        pushViewController(viewController, animated: true)
+        CATransaction.commit()
+    }
+}
