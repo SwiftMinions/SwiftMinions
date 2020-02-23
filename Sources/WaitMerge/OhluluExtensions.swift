77b4a9cf64df9a1d8bf44d7ee2dcb4356cb3cce7
ohlulu <z30262226@gmail.com>
2020-02-23 17:48:08 +0800

    [ Add ]：UIViewController

--- Sources/WaitMerge/OhluluExtensions.swift
+++ Sources/WaitMerge/OhluluExtensions.swift
@@ -954,3 +954,42 @@ public extension UIStackView {
         }
     }
 }
+
+public extension UIViewController {
+    
+    @objc func dismiss(_ animated: Bool = true, _ completion: (() -> Void)? = nil) {
+        dismiss(animated: animated, completion: completion)
+    }
+    
+    @objc func present(_ vc: UIViewController, animated: Bool = true, _ completion: (() -> Void)? = nil) {
+        present(vc, animated: animated, completion: completion)
+    }
+}
+
+public extension UIViewController {
+    
+    var isPresent: Bool {
+        if let navigationController = navigationController {
+            if navigationController.viewControllers.first == self {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+public extension UIViewController {
+    func addChild(viewController child: UIViewController, containerView: UIView) {
+        addChild(child)
+        containerView.bounds = child.view.bounds
+        containerView.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    
+    func removeChild(viewController child: UIViewController) {
+        guard parent != nil else { return }
+        child.willMove(toParent: nil)
+        child.view.removeFromSuperview()
+        child.removeFromParent()
+    }
+}
