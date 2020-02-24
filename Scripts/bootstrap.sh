f981320f28520c04e9dcf9381088028da60f1cde
stephen <tasb00429@gmail.com>
2020-02-24 22:27:46 +0800

    [ Add ]：Readme for descript what's going on inside script folders, [ Update ]：Update scripts file execute path

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-source $PWD/Scripts/not_so_important.sh
+source ./not_so_important.sh
 
 if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
   printf "${RED}Dependencies out of date with cache.${NC} Bootstrapping...\n"
-  carthage bootstrap --platform iOS
+  carthage bootstrap --project-directory $PWD/../ --platform iOS
 else
   printf "${GREEN}Cache up-to-date.${NC} Skipping bootstrap...\n"
 fi
