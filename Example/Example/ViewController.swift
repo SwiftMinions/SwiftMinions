e890ef3e9e9c512401918ff1ccb7fdb40bd571d3
stephen <tasb00429@gmail.com>
2020-02-24 21:27:54 +0800

    [ Update ]：Fit lints needs

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -14,10 +14,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-    
     }
-
-
 }
-
