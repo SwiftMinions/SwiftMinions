f77b9357ac95e81525fc116a347786c673c9f429
Natalie <nwy0206@gmail.com>
2020-02-28 12:13:34 +0800

    [ Add ]: JSONDecoder key decoding strategy extension

--- Sources/WaitMerge/NatalieExtensions.swift
+++ Sources/WaitMerge/NatalieExtensions.swift
@@ -111,3 +111,117 @@ extension UIDevice {
     }
 
 }
+
+struct CustomCodingKey: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init(_ base: CodingKey) {
+        self.init(stringValue: base.stringValue, intValue: base.intValue)
+    }
+
+    init(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    init(intValue: Int) {
+        self.stringValue = "\(intValue)"
+        self.intValue = intValue
+    }
+
+    init(stringValue: String, intValue: Int?) {
+        self.stringValue = stringValue
+        self.intValue = intValue
+    }
+}
+
+extension JSONDecoder.KeyDecodingStrategy {
+
+    /**
+     Convert from the first letter of key to lower case before attempting to match a key with the one specified by each type. Mainly designed for coverting the upper camel case to lower camel case.
+
+     ### Chinese description
+     把所有的 key 的第一個字母轉成小寫
+
+     ### Usage Example
+     ```
+     let decoder = JSONDecoder()
+     decoder.keyDecodingStrategy = .convertFirstLetterToLowerCase
+     ```
+     For example, `UserName` becomes `userName`, `Latest_Price` becomes  `latest_Price`.
+     */
+    static var convertFirstLetterToLowerCase: JSONDecoder.KeyDecodingStrategy {
+        return .custom { codingKeys in
+
+            var key = CustomCodingKey(codingKeys.last!)
+
+            if let firstChar = key.stringValue.first {
+                let i = key.stringValue.startIndex
+                key.stringValue.replaceSubrange(
+                    i...i, with: String(firstChar).lowercased()
+                )
+            }
+            return key
+        }
+    }
+
+
+    /**
+     Convert from the first letter of key to upper case before attempting to match a key with the one specified by each type. Mainly designed for coverting the lower camel case to upper camel case.
+
+     ### Chinese description
+     把所有的 key 的第一個字母轉成大寫
+
+     ### Usage Example
+     ```
+     let decoder = JSONDecoder()
+     decoder.keyDecodingStrategy = .convertFirstLetterToUpperCase
+     ```
+     For example, `userName` becomes `UserName`,`latest_price` becomes  `Latest_price`.
+     */
+    static var convertFirstLetterToUpperCase: JSONDecoder.KeyDecodingStrategy {
+        return .custom { codingKeys in
+
+            var key = CustomCodingKey(codingKeys.last!)
+
+            if let firstChar = key.stringValue.first {
+                let i = key.stringValue.startIndex
+                key.stringValue.replaceSubrange(
+                    i...i, with: String(firstChar).uppercased()
+                )
+            }
+            return key
+        }
+    }
+
+
+    /**
+     Convert from the key to lower case before attempting to match a key with the one specified by each type.
+
+     ### Chinese description
+     把所有的 key 轉成小寫，做到類似無視大小寫的效果
+
+     ### Usage Example
+     ```
+     let decoder = JSONDecoder()
+     decoder.keyDecodingStrategy = .convertToLowerCase
+     ```
+
+     For example, `userName` becomes `username`, `First_Day` becomes  `first_day`.
+     */
+    static var convertToLowerCase: JSONDecoder.KeyDecodingStrategy {
+        return .custom { codingKeys in
+
+            var key = CustomCodingKey(codingKeys.last!)
+
+            let start = key.stringValue.startIndex
+            let end = key.stringValue.endIndex
+            key.stringValue.replaceSubrange(
+                start..<end, with: key.stringValue.lowercased()
+            )
+
+            return key
+        }
+    }
+
+}
