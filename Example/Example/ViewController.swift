4c434d65fb5ef52f450a7cf93d3671c914c9f5c3
stephen <tasb00429@gmail.com>
2020-03-12 22:41:46 +0800

    [ Update ]：Hotfix 0.0.6

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,8 +11,19 @@ import SwiftMinions
 
 class ViewController: UIViewController {
 
+    private let testLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 14.0)
+        lbl.textColor = .black
+        lbl.text = "NSTextAlignment NSTextAlignment NSTextAlignment"
+        lbl.textAlignment = .center
+        return lbl
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let height = testLabel.calculateHeight(width: UIFont.systemFontSize)
+        print(height)
         // Do any additional setup after loading the view.
     }
 }
--- Sources/SwiftMinions/UIKit/LabelExtensions.swift
+++ Sources/SwiftMinions/UIKit/LabelExtensions.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UILabel {
+public extension UILabel {
     
     /**
      Calculate the height of label in a width.
