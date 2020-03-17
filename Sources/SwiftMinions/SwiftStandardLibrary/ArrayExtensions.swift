4a17d8a905664356af38a3c34a4f7c627b1e42da
Hank <hank.lu@silkrode.com>
2020-03-17 14:06:45 +0800

    [ Update ]：ArrayExtensions Chinese description markdown

--- Sources/SwiftMinions/SwiftStandardLibrary/ArrayExtensions.swift
+++ Sources/SwiftMinions/SwiftStandardLibrary/ArrayExtensions.swift
@@ -11,18 +11,26 @@ import Foundation
 public extension Array {
     
     /**
+     Shift elements in array by index
      ### Chinese description
-     位移陣列內的元素
+     將陣列裡的元素移動到 後綴(正數) or 前綴(負數)
      
      ## Usage Example: ##
      ```
      var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
      
+     // 移到後綴
      array = array.shift(withDistance: 3)
-     ```
      print(array) // prints: [4, 5, 6, 7, 8, 9, 10, 1, 2, 3]
+     
+     // 移到前綴
+     array = array.shift(withDistance: -3)
+     print(array) // prints: [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
+     ```
+     - Parameter distance: 從左邊開始數, 要移動幾個目標
+     - Returns: 已將目標移動到後綴或前綴的陣列.
      */
-    func shift(withDistance distance: Int = 1) -> Array<Element> {
+    func shift(with distance: Int = 1) -> Array<Element> {
         let offsetIndex = distance >= 0 ?
             self.index(startIndex, offsetBy: distance, limitedBy: endIndex) :
             self.index(endIndex, offsetBy: distance, limitedBy: startIndex)
@@ -32,24 +40,31 @@ public extension Array {
     }
     
     /**
+     Shift elements in array by index
      ### Chinese description
-     位移陣列內的元素
+     將陣列裡的元素移動到 後綴(正數) or 前綴(負數), 無回傳值
      
      ## Usage Example: ##
      ```
      var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-     array = array.shiftInPlace(withDistance: -2)
+     // 移到後綴
+     array.shift(withDistance: 3)
+     print(array) // prints: [4, 5, 6, 7, 8, 9, 10, 1, 2, 3]
+     
+     // 移到前綴
+     array.shiftInPlace(withDistance: -2)
      print(array) // prints: [9, 10, 1, 2, 3, 4, 5, 6, 7, 8]
      ```
+     - Parameter distance: 正數由陣列first, 負數由陣列last, 從左開始數要移動幾個目標
      */
-    mutating func shiftInPlace(withDistance distance: Int = 1) {
-        self = shift(withDistance: distance)
+    mutating func shiftInPlace(with distance: Int = 1) {
+        self = shift(with: distance)
     }
     
     
     /**
      ### Chinese description
-     移動陣列內的元素
+     將陣列裡的元素移動到 指定的位子
      
      ## Usage Example: ##
      ````
@@ -61,7 +76,7 @@ public extension Array {
      - Parameter toIndex: 要移動的目的地.
      - Returns: 已將目標移動完的陣列.
      */
-    func rearrange<T>(fromIndex: Int, toIndex: Int) -> Array<T>{
+    func rearrange<T>(fromIndex: Int, toIndex: Int) -> Array<T> {
         var arr = self
         let element = arr.remove(at: fromIndex)
         arr.insert(element, at: toIndex)
