cc0fdbf2884069da2f2e5ab682c55b8359b78620
stephen <tasb00429@gmail.com>
2020-03-22 01:25:42 +0800

    [ Remove ] ：Objects

--- Sources/WaitMerge/StephenObjects.swift
+++ Sources/WaitMerge/StephenObjects.swift
@@ -9,48 +9,7 @@
 import UIKit
 import Foundation
 
-/// Provide offen usage relevant system functions
-public struct ALSystems {
-    
-    /// 快速拿到值從Plist檔案
-    ///
-    /// - Parameter key: key in info.plist
-    /// - Returns:       value from key in info.plist
-    public static func getValueFromPlist(key: String) -> Any? {
-        return Bundle.main.object(forInfoDictionaryKey: key)
-    }
-    
-    /// App 當前的版本
-    public static let appVersion: String? = getValueFromPlist(key: "CFBundleShortVersionString") as? String
-    
-    /// App Build 的版號
-    public static let appBuildNumber: String? = getValueFromPlist(key: "CFBundleVersion") as? String
-    
-    /// App 的 BundleIdentifier
-    public static let appBundleID: String? = Bundle.main.bundleIdentifier
-    
-    /// An alphanumeric string that uniquely identifies a device to the app’s vendor.
-    public static let deviceID: String? = UIDevice.current.identifierForVendor?.uuidString
-   
-    /// Open application setting page
-    @available(iOS 10.0, *)
-    public static func openSetting() -> Void {
-        if let aUrl = URL(string: UIApplication.openSettingsURLString) {
-            UIApplication.shared.open(aUrl, options: [:], completionHandler: nil)
-        }
-    }
-}
 
-
-
-public func ifLet<T, U>(value: T?,
-                 success thenFunc: (T) -> (U),
-                 fail elseFunc: () -> (U)) -> U {
-    switch value {
-    case .some(let x): return thenFunc(x)
-    case .none: return elseFunc()
-    }
-}
 
 
 /// Provide offen usage relevant screen functions
