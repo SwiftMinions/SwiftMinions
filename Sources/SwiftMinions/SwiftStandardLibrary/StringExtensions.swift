2eebde363bb444ed675c76f3e17483eb17fdd963
ohlulu <z30262226@gmail.com>
2020-03-29 03:01:11 +0800

    [ Add ] : String extensions, base64 decoding & encoding

--- Sources/SwiftMinions/SwiftStandardLibrary/StringExtensions.swift
+++ Sources/SwiftMinions/SwiftStandardLibrary/StringExtensions.swift
@@ -61,4 +61,65 @@ public extension String {
         let size = CGSize(width:rect.size.width, height : rect.size.height)
         return size
     }
+    
+    var toInt: Int? {
+        return Int(self)
+    }
+    
+    var toIntValue: Int {
+        return Int(self) ?? 0
+    }
+    
+    /**
+     String to base64.
+
+     ## Chinese description
+     將字串編碼成 base64
+
+     ## Use example
+     ```swift
+        "SwiftMinions".base64Encoded()
+        // "U3dpZnRNaW5pb25z"
+     ```
+
+     - Parameters:
+        - encoding: String.Encoding.
+     - Returns: A base64Encoded string.
+    */
+    func base64Encoded(encoding: String.Encoding = .utf8) -> String? {
+        let plainData = data(using: .utf8)
+        return plainData?.base64EncodedString()
+    }
+    
+    /**
+     Base64 to string.
+
+     ## Chinese description
+     將字串用 base64 解碼
+
+     ## Use example
+     ```swift
+        "U3dpZnRNaW5pb25z".base64Encoded()
+        // "SwiftMinions"
+     ```
+
+     - Parameters:
+        - decoding: String.Dncoding. (default .utf8)
+        - options: Data.Base64DecodingOptions. (default .ignoreUnknownCharacters)
+     - Returns: A string decoding with base64.
+    */
+    func base64Decoded(decoding: String.Encoding = .utf8,
+                       options: Data.Base64DecodingOptions = .ignoreUnknownCharacters) -> String? {
+        let remainder = count % 4
+
+        var padding = ""
+        if remainder > 0 {
+            padding = String(repeating: "=", count: 4 - remainder)
+        }
+
+        guard let data = Data(base64Encoded: self + padding,
+                              options: options) else { return nil }
+
+        return String(data: data, encoding: decoding)
+    }
 }
