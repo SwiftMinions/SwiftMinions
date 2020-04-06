ac5aa3ae219d56761ec1d818b6d311906e9bdaca
Natalie <nwy0206@gmail.com>
2020-04-06 11:12:59 +0800

    [ Update ]: Add function in Float extension

--- Sources/SwiftMinions/SwiftStandardLibrary/FloatExtensions.swift
+++ Sources/SwiftMinions/SwiftStandardLibrary/FloatExtensions.swift
@@ -45,4 +45,25 @@ public extension Float {
         return CGFloat(self)
     }
     #endif
+
+
+    /**
+     This function returns a Float number rounded correctly to a given number of digits using the given rounding mode, which can be rounded, round up, round down or round-to-even
+
+     ## Chinese description
+     用指定方法（四捨五入/無條件進位/無條件捨去/四捨五入取偶數）取至小數位第x位
+
+     ## Usage Example
+     ```
+     let num: Float = 6.4456
+     num.rounded(to: 2, option: .down) // 6.45
+     num.rounded(to: 2, option: .up) // 6.45
+     num.rounded(to: 2, option: .down) // 6.44
+     num.rounded(to: 2, option: .bankers) // 6.45
+     ```
+     */
+    func rounded(to places: Int, option: NSDecimalNumber.RoundingMode) -> Float {
+        return self.doubleValue.rounded(to: places, option: option).floatValue
+    }
+
 }
