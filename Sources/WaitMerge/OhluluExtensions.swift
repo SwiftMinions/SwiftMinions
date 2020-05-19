f63905351c739648b5543d0e941be9330dbc00b8
ohlulu <z30262226@gmail.com>
2020-05-19 11:55:04 +0800

    [ Add ] : UIView animation extensions

--- Sources/WaitMerge/OhluluExtensions.swift
+++ Sources/WaitMerge/OhluluExtensions.swift
@@ -48,51 +48,5 @@ public extension UIViewController {
 }
 
 public extension UIView {
-    enum AnimationType {
-        case fadeOut
-        case fadeIn
-    }
-
-    /// Animation type
-    /// - Parameter type: set this value with AnimationType
-    /// - Parameter duration: set this value to duration (default is 0.25)
-    /// - Parameter delay: set this value to delay (default is 0)
-    /// - Parameter completion:  optional completion handler (default is nil).
-    func animation(
-        type: AnimationType,
-        duration: TimeInterval = 0.25,
-        delay: TimeInterval = 0,
-        _ completion: ((Bool) -> Void)? = nil) {
-        UIView.animate(withDuration: duration, delay: delay, animations: {
-            self.alpha = type == .fadeOut ? 0 : 1
-        }, completion: completion)
-    }
-
-    /// Animated with a horizontal shake, duration -> 0.35s
-    /// - duration -> 0.35s
-    /// - offsets -> [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, 0.0]
-    func errorshake(completion:(() -> Void)? = nil) {
-        shake(duration: 0.35,
-              offsets: [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, 0.0],
-              completion: completion)
-    }
     
-    func shake(duration: CFTimeInterval,
-               offsets: [CGFloat],
-               completion:(() -> Void)? = nil) {
-        constraints.forEach { $0.isActive = false }
-        translatesAutoresizingMaskIntoConstraints = true
-        CATransaction.begin()
-        let animation: CAKeyframeAnimation
-        
-        animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-        
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeIn)
-        
-        CATransaction.setCompletionBlock(completion)
-        animation.duration = duration
-        animation.values = offsets
-        layer.add(animation, forKey: "shake")
-        CATransaction.commit()
-    }
 }
