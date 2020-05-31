0053ae39733a686eb12a74fab16f1126796f6805
ohlulu <z30262226@gmail.com>
2020-05-31 17:58:08 +0800

    [ Add ] : URL extensions

--- Sources/WaitMerge/StephenExtensions.swift
+++ Sources/WaitMerge/StephenExtensions.swift
@@ -95,60 +95,7 @@ extension NSObject {
     }
 }
 
-extension URL {
-    
-    /// Get url query value via key
-    ///
-    /// If the URL is **http://www.google.com?AAAA=1111&BBBB=2222&CCCC=3333**, then give **AAAA** as key, its return **1111** .then give **BBBB** as key, its return **2222**.
-    ///
-    /// - Parameters:
-    ///   - aURL: url string
-    ///   - key: a string key
-    /// - Returns: correspond value via given key
-    public func getQueryItem(aURL: String?, key: String) -> String? {
-        
-        guard let aURLString = aURL else { return nil }
-        
-        let queryItems = URLComponents(string: aURLString)?.queryItems
-        let param = queryItems?.filter({$0.name == key}).first
-        
-        return param?.value
-    }
-    
-    /// Appending query string
-    ///
-    /// *** Example
-    ///
-    /// var url = URL(string: "https://www.example.com")
-    /// let finalURL = url?.appending("key1", value: "123")
-    ///                    .appending("key2", value: nil)
-    ///
-    /// ***
-    ///
-    /// - Parameters:
-    ///   - key: Key
-    ///   - value: Value
-    public func appending(_ key: String, value: String?) -> URL? {
 
-         guard var urlComponents = URLComponents(string: self.absoluteString) else { return self.absoluteURL }
-
-         // Create array of existing query items
-         var queryItems: [URLQueryItem] = urlComponents.queryItems ??  []
-
-         // Create query item
-         let queryItem = URLQueryItem(name: key, value: value)
-
-         // Append the new query item in the existing query items array
-         queryItems.append(queryItem)
-
-         // Append updated query items array in the url component object
-         urlComponents.queryItems = queryItems
-
-         // Returns the url from new url components
-         return urlComponents.url
-     }
-
-}
 /// Int 的擴充 方法 以及 參數
 extension Int {
     
