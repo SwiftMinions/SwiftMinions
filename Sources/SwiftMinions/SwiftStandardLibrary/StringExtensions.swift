da8d3b3c7329a20717a1b76d20ab478fd5f33cd5
stephen <tasb00429@gmail.com>
2020-08-24 11:25:13 +0800

    [ Add ]：RandomNonceString

--- Sources/SwiftMinions/SwiftStandardLibrary/StringExtensions.swift
+++ Sources/SwiftMinions/SwiftStandardLibrary/StringExtensions.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CryptoKit
 
 // MARK: - Helper method
 
@@ -231,6 +232,55 @@ public extension String {
                               options: options) else { return nil }
 
         return String(data: data, encoding: decoding)
+    }
+    
+     /**
+     Generate a random string—a "nonce" with ios build-in cryptographically secure nonce functions
+     
+     Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+     and
+     https://firebase.google.com/docs/auth/ios/apple
+     
+     ### Chinese description
+     透過 Apple 內建 SecRandomCopyBytes 方法產生安全的隨機字串
+     
+     ### Use example
+     ```swift
+         String().randomNonceString()
+         "".randomNonceString()
+         "".randomNonceString(length: 16)
+     ```
+     
+     - Parameters:
+        - length: lenght of nonce
+     - Returns: Nonce string
+    */
+    func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 { return }
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
     }
 }
 
